c90603170f013bb90993519d63f054c95ecf0a9f
Emil Tschenka Jensen <emil_jets@live.dk>
2022-02-18 11:44:25 +0100

    Emil ændring 1

--- HEJ/src/DevEmilApp.java
+++ HEJ/src/DevEmilApp.java
@@ -3,5 +3,6 @@ public class DevEmilApp {
     public static void main(String[] args) {
         System.out.println("This");
         System.out.println("kasdnksdj");
+        System.out.println("HEJ");
     }
 }
