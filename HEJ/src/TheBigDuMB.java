f61eda8779d3f3d4b9e2dba0f76a86c70666c409
Alin <alin_constantin15@hotmail.com>
2022-02-18 11:26:08 +0100

    rqwe

--- HEJ/src/TheBigDuMB.java
+++ HEJ/src/TheBigDuMB.java
@@ -2,9 +2,11 @@ public class TheBigDuMB {
     public static void main(String[] args) {
         System.out.println("mE sMArT");
         System.out.println("Alin baiter igen igen");
+        System.out.println("Emil er en kæmpe fucking boomer");
         System.out.println("Skriv et eller andet på linje 5");
         System.out.println("Bøøøøøøøh");
         System.out.println("lol");
         System.out.println("nanaaana");
+
     }
 }
