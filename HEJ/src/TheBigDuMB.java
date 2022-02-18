6c1c855b466eda18971ad4579b5fce0a1fb68504
Morten Karlsen <eaamkarls@students.eaaa.dk>
2022-02-18 11:24:18 +0100

    Fjerde test

--- HEJ/src/TheBigDuMB.java
+++ HEJ/src/TheBigDuMB.java
@@ -2,6 +2,7 @@ public class TheBigDuMB {
     public static void main(String[] args) {
         System.out.println("mE sMArT");
         System.out.println("Alin baiter igen igen");
+        System.out.println("Skriv et eller andet på linje 5");
         System.out.println("Bøøøøøøøh");
         System.out.println("lol");
     }
