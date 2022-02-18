3d9984f3879edd3952175bf78a9bc5e000706694
ellog <ellogovnor47@gmail.com>
2022-02-18 11:24:22 +0100

    test merge

--- HEJ/src/TheBigDuMB.java
+++ HEJ/src/TheBigDuMB.java
@@ -2,5 +2,6 @@ public class TheBigDuMB {
     public static void main(String[] args) {
         System.out.println("mE sMArT");
         System.out.println("lol");
+        System.out.println("Ronnie");
     }
 }
